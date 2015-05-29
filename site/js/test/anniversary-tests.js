8dbd527cceb3a2fcc7866d7993cc0df8c6964aa7
ayeseeem <ayeseeem@gmail.com>
2015-05-29 16:13:23 +0100

    Test is_celebratable_number

--- site/js/test/anniversary-tests.js
+++ site/js/test/anniversary-tests.js
@@ -10,9 +10,21 @@
     assert.ok(AYESEEEM !== undefined);
   });
 
-  QUnit.test("Example top-level anniversary test", function (assert) {
-    var dummy = AYESEEEM.anniversary.dummy;
-    assert.equal(dummy(), 123);
+  QUnit.test("is_celebratable_number", function (assert) {
+    var test = AYESEEEM.anniversary.is_celebratable_number;
+
+    assert.equal(test(6), 6);
+    assert.equal(test(7), 7);
+    assert.equal(test(8), 8);
+
+    assert.equal(test(69), 0);
+    assert.equal(test(70), 70);
+    assert.equal(test(71), 0);
+
+    assert.equal(test(699), 0);
+    assert.equal(test(700), 700);
+    assert.equal(test(701), 0);
+
   });
 
 }());
