2ec9992e1434371a037f8963482c42c00265eb78
ayeseeem <ayeseeem@gmail.com>
2015-05-29 17:28:21 +0100

    Fix test

--- site/js/test/anniversary-tests.js
+++ site/js/test/anniversary-tests.js
@@ -11,19 +11,19 @@
   });
 
   QUnit.test("is_celebratable_number", function (assert) {
-    var test = AYESEEEM.anniversary.is_celebratable_number;
+    var is_celebratable_number = AYESEEEM.anniversary.is_celebratable_number;
 
-    assert.equal(test(6), 6);
-    assert.equal(test(7), 7);
-    assert.equal(test(8), 8);
+    assert.equal(is_celebratable_number(6), 6);
+    assert.equal(is_celebratable_number(7), 7);
+    assert.equal(is_celebratable_number(8), 8);
 
-    assert.equal(test(69), 0);
-    assert.equal(test(70), 70);
-    assert.equal(test(71), 0);
+    assert.equal(is_celebratable_number(69), 0);
+    assert.equal(is_celebratable_number(70), 70);
+    assert.equal(is_celebratable_number(71), 0);
 
-    assert.equal(test(699), 0);
-    assert.equal(test(700), 700);
-    assert.equal(test(701), 0);
+    assert.equal(is_celebratable_number(699), 0);
+    assert.equal(is_celebratable_number(700), 700);
+    assert.equal(is_celebratable_number(701), 0);
 
   });
 
