4cb81af22ad11f8b12094daf20ba74133872c19e
ayeseeem <ayeseeem@gmail.com>
2022-12-22 16:49:29 +0000

    doc: fix table test description

--- site/js/test/table-tests.js
+++ site/js/test/table-tests.js
@@ -12,17 +12,17 @@
     assert.equal(123, 123);
   });
 
-  QUnit.test('HTML fixture is set up correctly', function (assert) {
+  QUnit.test('basic table read works', function (assert) {
     const dom = document.getElementById('qunit-fixture');
     const testTableHtml = `
     <table id="test-table">
       <thead>
-        <tr><th>Quantity</th>  <th>Currency</th><th>Price</th><th>Date</th>      <th>Source      </th></tr>
+        <tr><th>Quantity</th>  <th>Currency</th><th>Price</th><th>Date</th><th>Source</th>  </tr>
       </thead>
       <tbody>
-        <tr><td>pack of 20</td><td>GBP</td>     <td>6.00</td><td>2010-02-16</td><td>            </td></tr>
-        <tr><td>pack of 20</td><td>GBP</td>     <td>8.80</td><td>2014-10-18</td><td>            </td></tr>
-        <tr><td>pack of 20</td><td>GBP</td>     <td>8.96</td><td>2015-05-29</td><td>            </td></tr>
+        <tr><td>pack of 20</td><td>GBP</td><td>6.00</td><td>2010-02-16</td><td>Source 1</td></tr>
+        <tr><td>pack of 20</td><td>GBP</td><td>8.80</td><td>2014-10-18</td><td>Source 2</td></tr>
+        <tr><td>pack of 20</td><td>GBP</td><td>8.96</td><td>2015-05-29</td><td>Source 3</td></tr>
       </tbody>
     </table>
     `;
