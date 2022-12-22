8ebb304f4d2e937246b739b0c6cfe705e5009870
ayeseeem <ayeseeem@gmail.com>
2022-12-22 17:36:19 +0000

    test: more table getData() tests

--- site/js/test/table-tests.js
+++ site/js/test/table-tests.js
@@ -57,6 +57,79 @@
       { 'date': '2014-10-18', 'price': '8.80' },
       { 'date': '2015-05-29', 'price': '8.96'}
     ]);
+
+    assert.deepEqual(getData(testTable, 3), [
+      { 'date': '2015-05-29', 'price': '8.96'}
+    ]);
+  });
+
+  QUnit.test('excludes all rows if header count matches row count', function (assert) {
+    document.getElementById('qunit-fixture').innerHTML = `
+    <table id="test-table">
+      <thead>
+        <tr><th>Quantity</th><th>Currency</th><th>Price</th><th>Date</th><th>Source</th></tr>
+      </thead>
+      <tbody>
+        <tr><td>Thing 1</td><td>GBP</td><td>6.00</td><td>2010-02-16</td><td>Source 1</td></tr>
+        <tr><td>Thing 2</td><td>GBP</td><td>8.80</td><td>2014-10-18</td><td>Source 2</td></tr>
+        <tr><td>Thing 3</td><td>GBP</td><td>8.96</td><td>2015-05-29</td><td>Source 3</td></tr>
+      </tbody>
+    </table>
+    `;
+
+    const testTable = document.getElementById('test-table');
+
+    assert.equal(testTable.rows.length, 4);
+    assert.deepEqual(getData(testTable, 4), [
+    ]);
+  });
+
+  QUnit.test('excludes all rows if header count *exceeds* row count', function (assert) {
+    document.getElementById('qunit-fixture').innerHTML = `
+    <table id="test-table">
+      <thead>
+        <tr><th>Quantity</th><th>Currency</th><th>Price</th><th>Date</th><th>Source</th></tr>
+      </thead>
+      <tbody>
+        <tr><td>Thing 1</td><td>GBP</td><td>6.00</td><td>2010-02-16</td><td>Source 1</td></tr>
+        <tr><td>Thing 2</td><td>GBP</td><td>8.80</td><td>2014-10-18</td><td>Source 2</td></tr>
+        <tr><td>Thing 3</td><td>GBP</td><td>8.96</td><td>2015-05-29</td><td>Source 3</td></tr>
+      </tbody>
+    </table>
+    `;
+
+    const testTable = document.getElementById('test-table');
+
+    assert.equal(testTable.rows.length, 4);
+    assert.deepEqual(getData(testTable, 5), [
+    ]);
+  });
+
+  QUnit.test('full table extracts header labels too', function (assert) {
+    document.getElementById('qunit-fixture').innerHTML = `
+    <table id="test-table">
+      <thead>
+        <tr><th>Quantity</th><th>Currency</th><th>Price</th><th>Date</th><th>Source</th></tr>
+      </thead>
+      <tbody>
+        <tr><td>Thing 1</td><td>GBP</td><td>6.00</td><td>2010-02-16</td><td>Source 1</td></tr>
+        <tr><td>Thing 2</td><td>GBP</td><td>8.80</td><td>2014-10-18</td><td>Source 2</td></tr>
+        <tr><td>Thing 3</td><td>GBP</td><td>8.96</td><td>2015-05-29</td><td>Source 3</td></tr>
+      </tbody>
+    </table>
+    `;
+
+    const testTable = document.getElementById('test-table');
+
+    assert.deepEqual(getData(testTable, 0)[0],
+      { 'date': 'Date',       'price': 'Price' }
+    );
+    assert.deepEqual(getData(testTable, 0), [
+      { 'date': 'Date',       'price': 'Price' },
+      { 'date': '2010-02-16', 'price': '6.00' },
+      { 'date': '2014-10-18', 'price': '8.80' },
+      { 'date': '2015-05-29', 'price': '8.96'}
+    ]);
   });
 
 }());
