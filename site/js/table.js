a00e79c1a1cc3fa4ac0fd8c8c2d7d50768279b3b
ayeseeem <ayeseeem@gmail.com>
2022-12-25 15:07:43 +0000

    feat: add getDataGrid()

--- site/js/table.js
+++ site/js/table.js
@@ -6,6 +6,13 @@ var AYESEEEM;
 AYESEEEM = (function (module) {
   'use strict';
 
+  /**
+   * Gets rows of data from table as objects.
+   * @param {HTMLTableElement} table table to get data from
+   * @param {Integer} headerRowsCount number of header rows
+   * @returns an array of objects
+   */
+  // TODO: ICM 2022-12-22: Refactor to use getData()
   function getData(table, headerRowsCount) {
     // TODO: ICM 2022-12-19: Extract column headers from table
     const priceColumnIndex = 2;
@@ -36,12 +43,47 @@ AYESEEEM = (function (module) {
   }
 
   /**
+   * Gets rows of data from table as arrays.
+   * @param {HTMLTableElement} table table to get data from
+   * @param {Integer} headerRowsCount number of header rows
+   * @returns an array of arrays
+   */
+  function getDataGrid(table, headerRowsCount) {
+    const priceColumnIndex = 2;
+    const dateColumnIndex = 3;
+
+    const points = [];
+    const rowCount = table.rows.length;
+    for (var i = 0 + headerRowsCount; i < rowCount; i++) {
+      const cells = table.rows.item(i).cells;
+
+      const point = [];
+      const cellCount = cells.length;
+      for (var j = 0; j < cellCount; j++){
+        const cellVal = cells.item(j).innerHTML;
+
+        if (j === priceColumnIndex) {
+          point.push(cellVal);
+        }
+
+        if (j === dateColumnIndex) {
+          point.push(cellVal);
+        }
+      }
+      points.push(point);
+    }
+
+    return points;
+  }
+
+  /**
    * A "dumber" version that just reads (all) the data. Let others process it
    * later.
    *
    * @param {*} table table DOM element
    * @returns all the `innerHTML` of the cells, in an array of arrays
    */
+  // TODO: ICM 2022-12-25: Rename as getAllDataGrid()
   function getAllData(table) {
     // TODO: ICM 2022-12-22: Verify it's a table element
     const points = [];
@@ -63,6 +105,7 @@ AYESEEEM = (function (module) {
 
   module.table = {
     getData: getData,
+    getDataGrid: getDataGrid,
     getAllData: getAllData
   };
 
